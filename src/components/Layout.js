ed1857161c835b161b3865cb1ca49f45b5ad07ab
Mark Crisostomo <mark@deviseinteractive.com>
2019-04-01 13:46:52 -0700

    import caps fix for navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
 
-import Navbar from "../components/navbar/navbar";
+import Navbar from "../components/navbar";
 
 import "./all.sass";
 import "./layout.css";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,54 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import logo from "../../img/logo.png";
+import Button from "../Button/Button";
+
+const Logo = styled.img`
+  //width: 50px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const Navbar = class extends React.Component {
   componentDidMount() {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
+      document.querySelectorAll(".navbar-burger"),
       0
-    )
+    );
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
       // Add a click event on each of them
       $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
+        el.addEventListener("click", () => {
           // Get the target from the "data-target" attribute
-          const target = el.dataset.target
-          const $target = document.getElementById(target)
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
 
           // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active')
-          $target.classList.toggle('is-active')
-        })
-      })
+          el.classList.toggle("is-active");
+          $target.classList.toggle("is-active");
+        });
+      });
     }
   }
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
+      <nav className="navbar" role="navigation" aria-label="main-navigation">
+        <Container className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <Logo
+                src={logo}
+                alt="OC Tech Happy Hour"
+                width="50"
+                height="auto"
+              />
             </Link>
             {/* Hamburger menu */}
             <div className="navbar-burger burger" data-target="navMenu">
@@ -46,41 +57,16 @@ const Navbar = class extends React.Component {
               <span />
             </div>
           </div>
+
           <div id="navMenu" className="navbar-menu">
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+            <div className="navbar-end">
+              <Button className="navbar-item" text="Become a Member" />
             </div>
           </div>
-        </div>
+        </Container>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;

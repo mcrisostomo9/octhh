415706e44214a99121c0e52ae5c3f2f7763327d8
Mark Crisostomo <mark@deviseinteractive.com>
2019-04-01 14:06:20 -0700

    idk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
-import logo from "../../img/logo.png";
-import Button from "../Button/Button";
+import logo from "../img/logo.png";
+import Button from "./Button/Button";
 
 const Logo = styled.img`
   //width: 50px;

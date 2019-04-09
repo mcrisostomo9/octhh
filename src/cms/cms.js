8bd81bd3a0d09662040e93677b57d8d855cfd946
Mark Crisostomo <mark@deviseinteractive.com>
2019-04-09 11:12:30 -0700

    registered sponsorship page in admin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from "netlify-cms";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
 import HomePagePreview from "./preview-templates/HomePagePreview";
+import SponsorshipPagePreview from "./preview-templates/SponsorshipPagePreview";
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("home", HomePagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("sponsorship", SponsorshipPagePreview);

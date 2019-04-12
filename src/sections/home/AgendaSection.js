4e7dd24b0f8d44041e9e7c1fef510918ba96547a
Mark Crisostomo <mark@deviseinteractive.com>
2019-04-11 19:21:52 -0700

    fixed styling on agenda borders

--- src/sections/home/AgendaSection.js
+++ src/sections/home/AgendaSection.js
@@ -7,22 +7,26 @@ import fluidTypography from "../../utils/fluidTypography";
 
 const StyledSectionContainer = styled(SectionContainer)`
   background-color: #f2f2f2;
+  ${mq.a1200} {
+    padding-bottom: 40px;
+  }
 `;
 
 const ItemContainer = styled.div`
-  //margin-bottom: 1rem;
   display: grid;
   grid-template-columns: 1fr;
-  flex-direction: column;
   width: 100%;
   max-width: 1000px;
   margin: 0 auto 1rem;
   ${mq.a992} {
-    flex-direction: row;
     grid-template-columns: 1fr 1fr 2fr;
-    border-top: 1px #a3a3a3 solid;
+    border-top: ${props => (props.firstItem ? `1px #a3a3a3 solid` : `none`)};
     border-bottom: 1px #a3a3a3 solid;
     text-align: left;
+    align-items: center;
+    padding: 2rem 0;
+    grid-column-gap: 1rem;
+    margin-bottom: 0;
   }
 `;
 
@@ -30,6 +34,10 @@ const Time = styled.div`
   color: var(--blue);
   margin-bottom: 0.25rem;
   ${fluidTypography(320, 1980, 16, 30)};
+
+  ${mq.a992} {
+    margin-bottom: 0;
+  }
 `;
 
 const ItemTitle = styled.div`
@@ -55,13 +63,16 @@ const AgendaSection = ({ data: { agendaItems } }) => {
     <StyledSectionContainer>
       <SectionTitle title="Agenda" />
       <AgendaContainer>
-        {agendaItems.map(i => (
-          <ItemContainer>
-            <Time>{i.timeRange}</Time>
-            <ItemTitle>{i.title}</ItemTitle>
-            <ItemDescription>{i.agendaDescription}</ItemDescription>
-          </ItemContainer>
-        ))}
+        {agendaItems.map((agenda, index) => {
+          const firstItem = index === 0;
+          return (
+            <ItemContainer firstItem={firstItem}>
+              <Time>{agenda.timeRange}</Time>
+              <ItemTitle>{agenda.title}</ItemTitle>
+              <ItemDescription>{agenda.agendaDescription}</ItemDescription>
+            </ItemContainer>
+          );
+        })}
       </AgendaContainer>
     </StyledSectionContainer>
   );
